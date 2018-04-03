4e7c2a56c9bfc118e9b8187dc6df8c5745c17061
ronaldoaf <ronaldo.a.f@gmail.com>
2018-04-03 16:27:31 -0300

    Update jquery_tools.js

--- jquery_tools.js
+++ jquery_tools.js
@@ -1,13 +1,25 @@
 
 $.waitFor=function(elemento, func, timeout, func_error){
+    if( typeof timeout === "undefined" ) timeout=30;
+    if( typeof func_error === "undefined" ) func_error=function(){};
 	var c=0;
 	var loop=setInterval(function(){
 		if ($(elemento).size()>0) {
 			func();
 			clearInterval(loop);
 		}
-		if(c>=timeout)  clearInterval(loop);
+		if(c>=timeout)  {
+            clearInterval(loop);
+            func_error();
+        }
 		c+=0.5;
 
 	},500);
 }
+
+
+jQuery.fn.extend({
+  textOnly: function() {
+     return this.clone().children().remove().end().text();  
+   }
+});
