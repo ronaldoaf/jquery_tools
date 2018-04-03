dbef35dfdde69dde1dc7328b483808579b298c2c
ronaldoaf <ronaldo.a.f@gmail.com>
2018-04-03 15:13:32 -0300

    Update jquery_tools.js

--- jquery_tools.js
+++ jquery_tools.js
@@ -1,1 +1,13 @@
 
+$.waitFor=function(elemento, func, timeout, func_error){
+	var c=0;
+	var loop=setInterval(function(){
+		if ($(elemento).size()>0) {
+			func();
+			clearInterval(loop);
+		}
+		if(c>=timeout)  clearInterval(loop);
+		c+=0.5;
+
+	},500);
+}
